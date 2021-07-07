a3f16774d0977daaf250b26e16a0aba2417a9dd2
Oliver Winter <owinter86@gmail.com>
2021-07-07 18:42:58 +0200

    feat: add semantic releases main repo

---
+++ eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  root: true,
+  extends: '@lightbase/eslint-config-lightbase/rn',
+  env: {
+    jest: true,
+  },
+  rules: {
+    '@typescript-eslint/no-explicit-any': 0,
+  },
+};
