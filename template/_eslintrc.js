b9b16378a0e8aba23c0e10767a59295f76d08f7a
Oliver Winter <owinter86@gmail.com>
2021-07-07 18:43:38 +0200

    feat: add semantic releases and husky to template

--- template/_eslintrc.js
+++ template/_eslintrc.js
@@ -5,15 +5,6 @@ module.exports = {
     jest: true,
   },
   rules: {
-    'no-unused-vars': 'error',
-    '@typescript-eslint/no-explicit-any': [0],
-    'react-hooks/exhaustive-deps': [
-      1,
-      {
-        enableDangerousAutofixThisMayCauseInfiniteLoops: true,
-        additionalHooks:
-          '(useAnimatedStyle|useAnimatedProps|useDerivedValue|useAnimatedGestureHandler)',
-      },
-    ],
+    '@typescript-eslint/no-explicit-any': 0,
   },
 };
